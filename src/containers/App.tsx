2d012f349b76001700669635c9bbe910923e1dd9
Grzegorz Klimek <grzegorz@e-gaming.cz>
2017-01-04 13:40:21 +0100

    cleaning up

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
 import * as async from 'async';
 
-import { PropTypes } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-
-import actions from '../actions';
-
 import Nav from '../components/Nav';
 import Groups from '../components/Groups';
 import Message from '../components/Message';
 
-const App: any = () => {
+const App = () => {
   return (
     <div>
       <Nav/>
