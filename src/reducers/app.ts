c248b18afd1b83f21f7b4128540dccb4d767d746
Grzegorz Klimek <kfiku.com@gmail.com>
2017-01-10 02:04:39 +0100

    move to clone method

--- src/reducers/app.ts
+++ src/reducers/app.ts
@@ -2,6 +2,8 @@ import { ADD_REPO, ADDING_REPO, ADDING_REPO_END,
          RELOADING, UPDATE_REPO, DELETE_REPO, RELOADING_ALL_REPOS, RELOADING_ALL_REPOS_END,
          MESSAGE } from '../constants/ActionTypes';
 
+import clone from '../helpers/Clone';
+
 const initialState = {
   addingRepos: false,
   reloadingAllRepos: false,
@@ -12,27 +14,27 @@ export default function app(state = initialState, action) {
   let newState;
   switch (action.type) {
     case ADDING_REPO:
-      newState = JSON.parse(JSON.stringify(state));
+      newState = clone(state);
       newState.addingRepos = true;
       return newState;
 
     case ADDING_REPO_END:
-      newState = JSON.parse(JSON.stringify(state));
+      newState = clone(state);
       newState.addingRepos = false;
       return newState;
 
     case RELOADING_ALL_REPOS:
-      newState = JSON.parse(JSON.stringify(state));
+      newState = clone(state);
       newState.reloadingAllRepos = true;
       return newState;
 
     case RELOADING_ALL_REPOS_END:
-      newState = JSON.parse(JSON.stringify(state));
+      newState = clone(state);
       newState.reloadingAllRepos = false;
       return newState;
 
     case MESSAGE:
-      newState = JSON.parse(JSON.stringify(state));
+      newState = clone(state);
       newState.message = action.msg;
       return newState;
 
