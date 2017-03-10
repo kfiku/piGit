90b130884a9c6f94a0621937018e300c8ce12fb3
Grzegorz Klimek <grzegorz@e-gaming.cz>
2017-03-10 08:00:39 +0100

    add env versions log

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
+console.log(
+`node: ${process.versions.node}
+chrome: ${process.versions.chrome}
+electron: ${process.versions.electron}
+`);
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { createStore, StoreEnhancer } from 'redux';
