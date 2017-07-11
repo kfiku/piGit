7c69f75ce687862edd3566b584f84096cef61a8c
Grzegorz Klimek <kfiku.com@gmail.com>
2017-07-11 18:05:19 +0200

    update pkg

---
+++ src/components/helpers/Isvg.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import { readFileSync } from 'fs';
+
+import { renderLog } from '../../helpers/logger';
+
+interface IsvgProps {
+ src: string,
+}
+
+const Isvg: React.SFC<IsvgProps> = ({ src, ...rest }) => {
+  renderLog('Isvg src=' + src);
+  const svg = readFileSync('./src/svg/' + src).toString().replace('<?xml version="1.0" encoding="iso-8859-1"?>', '');
+  return (
+    <div {...rest} dangerouslySetInnerHTML={svg}>
+      {src}
+    </div>
+  );
+};
+
+export default Isvg;
