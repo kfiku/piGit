452a7486c6bf94dada9fb8773a6c2e8872e0d8b3
Grzegorz Klimek <kfiku.com@gmail.com>
2017-09-22 10:16:45 +0200

    add dialog styling

--- src/renderer/components/helpers/Confirm.tsx
+++ src/renderer/components/helpers/Confirm.tsx
@@ -1,13 +1,51 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+import { navHeight, g1, g2, g4, g7, defaultFont } from '../../utils/styles';
 import { renderLog } from '../../helpers/logger';
+
+const Dialog = styled.div`
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 100;
+`;
+
+const Content = styled.div`
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: fixed;
+  z-index: 101;
+  padding: 16px;
+
+
+  border-radius: $br * 2;
+  background-color: white;
+
+  p {
+    margin-top: 0;
+  }
+`;
+
+const Bg = styled.div`
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 100;
+  background-color: rgba(10, 10, 10, 0.86);
+`;
 
 const Confirm: any = ({msg, yes, no}) => {
   renderLog('DIALOG');
   return (
-    <div className='dialog confirm'>
-      <div className='dialog-content'>
+    <Dialog className='confirm'>
+      <Content className='dialog-content'>
         <p>{ msg }</p>
 
         <footer>
@@ -15,10 +53,10 @@ const Confirm: any = ({msg, yes, no}) => {
           { ' ' }
           <button className='btn' onClick={ no }>No</button>
         </footer>
-      </div>
+      </Content>
 
-      <div className='dialog-background'></div>
-    </div>
+      <Bg className='dialog-background'></Bg>
+    </Dialog>
   );
 };
 
