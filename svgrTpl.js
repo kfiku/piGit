3e52c8570efea6ed8079e2e6a0cb01dafd24d9bb
Grzegorz Klimek <kfiku.com@gmail.com>
2017-11-06 13:50:28 +0100

    update tpl

--- svgrTpl.js
+++ svgrTpl.js
@@ -1,7 +1,7 @@
 module.exports = () => (code, state) => `
 import * as React from 'react'
 
-export default function ${state.componentName}() {
+export default function ${state.componentName}(props) {
   /* tslint:disable:max-line-length */
   return ${code}
   /* tslint:enable:max-line-length */
