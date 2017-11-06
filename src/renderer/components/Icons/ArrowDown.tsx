2b03250c0c6b4bf8f09e912843546e97bc66a717
Grzegorz Klimek <kfiku.com@gmail.com>
2017-11-06 13:51:10 +0100

    update props orientation

--- src/renderer/components/Icons/ArrowDown.tsx
+++ src/renderer/components/Icons/ArrowDown.tsx
@@ -3,11 +3,11 @@ import * as React from 'react';
 export default function (props) {
   return (
     <svg
-      {...props}
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 490.4 490.4'
       width='32'
       height='32'
+      {...props}
     >
       <path d='M232.95 58.75v343.4l-128-128c-4.8-4.8-12.5-4.8-17.3 0-2.4 2.4-3.6 5.5-3.6 8.7 0 3.2 1.2 6.3 3.6 8.7l148.9 148.9c4.8 4.8 12.5 4.8 17.3 0l148.9-148.9c4.8-4.8 4.8-12.5 0-17.3-4.8-4.8-12.5-4.8-17.3 0l-128 128V58.75c0-6.8-5.5-12.3-12.3-12.3-6.8 0-12.2 5.5-12.2 12.3z'/> // tslint:disable-line
     </svg>
