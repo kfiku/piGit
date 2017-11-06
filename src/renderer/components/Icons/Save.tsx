d88c8c898654e26940f2396d420d5ae59c4fc961
Grzegorz Klimek <kfiku.com@gmail.com>
2017-11-06 13:50:40 +0100

    add icons

---
+++ src/renderer/components/Icons/Save.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default function RightArrow6(props) {
+  /* tslint:disable:max-line-length */
+  return (
+    <svg viewBox='0 0 54 54' {...props}>
+      <path d='M27 0C12.112 0 0 12.112 0 27s12.112 27 27 27 27-12.112 27-27S41.888 0 27 0zm0 52C13.215 52 2 40.785 2 27S13.215 2 27 2s25 11.215 25 25-11.215 25-25 25z' />
+      <path d='M23.001 14.293a.999.999 0 1 0-1.414 1.414L32.88 27 21.587 38.293a.999.999 0 1 0 1.414 1.414l11.498-11.498a1.711 1.711 0 0 0 0-2.418L23.001 14.293z' />
+    </svg>
+  );
+
+  /* tslint:enable:max-line-length */
+}
