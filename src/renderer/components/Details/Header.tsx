7d28bfde27a52188307da5206a4b5e8f55a7942b
Grzegorz Klimek <kfiku.com@gmail.com>
2017-11-12 21:43:21 +0100

    header do new irepo

--- src/renderer/components/Details/Header.tsx
+++ src/renderer/components/Details/Header.tsx
@@ -3,7 +3,7 @@ import { IRepo } from '../../interfaces/IRepo';
 import * as React from 'react';
 import styled from 'styled-components';
 
-import { g2, lh } from '../../utils/styles';
+import { g2, lh, detailsHeaderHeight } from '../../utils/styles';
 import Button from '../helpers/Button';
 import Icon from '../helpers/Icon';
 import ArrowLeft from '../Icons/ArrowLeft';
@@ -12,13 +12,12 @@ import ArrowDown from '../Icons/ArrowDown';
 import Reload from '../Icons/Reload';
 import Stats from '../Repos/Stats';
 
-// import GitGuiBtn from './GitGuiBtn';
 import GitKBtn from './GitKBtn';
 import TermBtn from './TermBtn';
 
 const StyledHeader = styled.header`
   display: flex;
-  height: 48px;
+  height: ${detailsHeaderHeight}px;
   align-items: center;
   justify-content: center;
 
@@ -51,6 +50,19 @@ function Header({ repo, actions }: HeaderProps) {
       </h2>
 
       <div className='buttons'>
+
+      {repo.stats.behind ? (
+          <Button onClick={actions.pullRepo.bind(null, repo.id, repo.dir)} title='Pull this repo'>
+            <ArrowDown />
+          </Button>
+        ) : repo.stats.ahead ? (
+          <Button
+            onClick={actions.pushRepo.bind(null, repo.id, repo.dir)}
+            title='Push to this repo'>
+            <ArrowUp />
+          </Button>
+        ) : null}
+
         <Button
           onClick={ actions.reloadRepo.bind(null, repo.id, repo.dir) } title='Refresh this repo'>
           <Icon spin={repo.progressing} className='icon icon-refresh'>
@@ -58,19 +70,6 @@ function Header({ repo, actions }: HeaderProps) {
           </Icon>
         </Button>
 
-        { repo.behind ? (
-          <Button onClick={ actions.pullRepo.bind(null, repo.id, repo.dir) } title='Pull this repo'>
-            <ArrowDown />
-          </Button>
-        ) : repo.ahead ? (
-          <Button
-          onClick={ actions.pushRepo.bind(null, repo.id, repo.dir) }
-          title='Push to this repo'>
-            <ArrowUp />
-          </Button>
-        ) : null }
-
-        {/* <GitGuiBtn repo={repo} /> */}
         <GitKBtn repo={repo} />
         <TermBtn repo={repo} />
       </div>
@@ -78,6 +77,5 @@ function Header({ repo, actions }: HeaderProps) {
     </StyledHeader>
   );
 }
-
 
 export default Header;
