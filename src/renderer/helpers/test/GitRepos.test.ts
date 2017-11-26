e68b8db8a9eff8d1e47e029459e71189858b5de9
Grzegorz Klimek <kfiku.com@gmail.com>
2017-11-27 00:15:17 +0100

    move test closer to code

--- src/renderer/helpers/test/GitRepos.test.ts
+++ src/renderer/helpers/test/GitRepos.test.ts
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
 import { join } from 'path';
-import gitRepos from '../../../src/renderer/helpers/GitRepos';
+import gitRepos from '../GitRepos';
 
 it('should proper search for repos', (done) => {
-  let gitDirsWonted = [join(__dirname, '..', '..', '..')];
+  let gitDirsWonted = [join(__dirname, '..', '..', '..', '..')];
 
   gitRepos.searchRepos(['.'], (dir) => {
     expect(dir).to.be.equal(gitDirsWonted[0]);
