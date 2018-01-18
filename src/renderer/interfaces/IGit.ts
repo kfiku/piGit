9a746c861213c433cb8ebf104ed5c758984b9b40
Grzegorz Klimek <kfiku.com@gmail.com>
2018-01-18 19:33:20 +0100

    update int

--- src/renderer/interfaces/IGit.ts
+++ src/renderer/interfaces/IGit.ts
@@ -12,13 +12,19 @@ export interface IStatusStats {
   modified: number;
   deleted: number;
   renamed: number;
+  added: number;
   untracked: number;
   conflicted: number;
 }
 
+
+export type IFileType = '?' | 'M' | 'A' | 'D' | 'R' | 'U' | 'C' | '';
+
 export interface IFile {
   path: string;
-  type: '?' | 'M' | 'A' | 'D' | 'R' | 'U' | 'C';
+  type: IFileType;
+  index?: IFileType;
+  workspace?: IFileType;
   staged: boolean;
   conflicted: boolean;
 }
